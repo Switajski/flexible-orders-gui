2ac12e1c932b77b1a5f7e6c1e2431007537f62ba
Marek Switajski <marek@Switajski.de>
2017-03-19 10:08:10 +0100

    Exploratory test for async actions added

--- src/test/reducer.test.js
+++ src/test/reducer.test.js
@@ -1,38 +1,48 @@
-import reducer from '../reducer';
-import documents from './testDocuments'
 import nock from 'nock'
-import { showDueItemsOnly, 
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import {
+    showDueItemsOnly,
     SHOWING_DUE_ITEMS_ONLY,
     FETCHING_DOCS,
     FETCH_DOCS_SUCCESS
- } from '../actions'
+} from '../actions'
+import reducer from '../reducer'
+import documents from './testDocuments'
 
 describe('reducer SHOWING_DUE_ITEMS_ONLY', () => {
     it('filter state should be set', () => {
         const reduced = reducer({ filter: [] }, showDueItemsOnly)
         expect(reduced).toEqual({ filter: [SHOWING_DUE_ITEMS_ONLY] })
-    })
+    }) 
 });
 
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
 describe('async actions', () => {
-  afterEach(() => {
-    nock.cleanAll()
-  })
+    afterEach(() => {
+        nock.cleanAll()
+    }) 
 
-  it('creates FETCH_DOCS_SUCCESS when fetching todos has been done', () => {
-    nock('http://localhost:3000')
-      .get('/documents')
-      .reply(200, { body: documents})
+    it('creates FETCH_DOCS_SUCCESS when fetching todos has been done', () => {
+        nock('http://localhost:3000')
+            .get('/documents')
+            .reply(200, { body: documents })
 
-    const expectedActions = [
-      { type: FETCHING_DOCS },
-      { type: FETCH_DOCS_SUCCESS, body: { documents: documents  } }
-    ]
-    const store = mockStore({ documents: [] })
+        const expectedActions = [
+            { type: FETCHING_DOCS },
+            {
+                type: FETCH_DOCS_SUCCESS,
+                body: { documents: documents }
+            }
+        ]
+        const store = mockStore({ documents: [] })
 
-    return store.dispatch()
-      .then(() => { // return of async actions
-        expect(store.getActions()).toEqual(expectedActions)
-      })
-  })
+        return store.dispatch()
+            .then(() => { // return of async actions
+                expect(store.getActions()).toEqual(expectedActions)
+            })
+    })
 })
