07d381be31022e719afe333789bac68e5d3464ab
Marek Switajski <marek@Switajski.de>
2017-03-31 18:31:33 +0200

    Autosuggestions reacts on input

--- src/QueryInput/reducer.js
+++ src/QueryInput/reducer.js
@@ -7,11 +7,52 @@ import {
 
 import { createNormalizedTestData } from '../DocumentList/test/testDocuments'
 
+//TODO: If customer would be also normalized, then much of following code could be saved.
 const { documents, lineItems } = createNormalizedTestData();
+
+const docArray = [];
+const customerArray = [];
+Object.keys(documents).forEach(key => {
+    const doc = documents[key];
+    docArray.push(doc)
+    if (doc.customer) {
+        const custWithId = { ...doc.customer, id: doc.customer.customerNumber }
+        if (!customerArray.includes(custWithId)) {
+            customerArray.push(custWithId)
+        }
+    }
+})
+
 const initialState = {
     suggestions: [],
     value: '',
-    documents: documents
+    keyValue: {
+        documentIds: docArray,
+        customerLastNames: customerArray
+    }
+}
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getMatchingDocumentIds(value, documents) {
+    const escapedValue = escapeRegexCharacters(value.trim());
+    if (escapedValue === '') {
+        return [];
+    }
+    const regex = new RegExp('^' + escapedValue, 'i');
+    return documents.filter(doc => regex.test(doc.id));
+}
+
+function getMatchingCustomerLastNames(value, customerLastNames) {
+    const escapedValue = escapeRegexCharacters(value.trim());
+    if (escapedValue === '') {
+        return [];
+    }
+    const regex = new RegExp('^' + escapedValue, 'i');
+    return customerLastNames.filter(doc => regex.test(doc.lastName));
 }
 
 export default (state = initialState, action) => {
@@ -29,9 +70,9 @@ export default (state = initialState, action) => {
             };
 
         case LOAD_SUGGESTIONS_BEGIN:
-            const suggestions = []
-            Object.keys(state.documents).forEach(key =>
-                suggestions.push(state.documents[key]))
+            const suggestions = getMatchingDocumentIds(state.value, state.keyValue.documentIds);
+            getMatchingCustomerLastNames(state.value, state.keyValue.customerLastNames).forEach(cln => suggestions.push(cln))
+
             return {
                 ...state,
                 isLoading: true,
