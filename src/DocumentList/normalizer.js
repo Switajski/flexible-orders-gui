3e306730f0dd4653d1b4ec8224b72876a65a7a24
Marek Switajski <marek@Switajski.de>
2017-04-01 12:33:41 +0200

    Normalized customers out of doc

--- src/DocumentList/normalizer.js
+++ src/DocumentList/normalizer.js
@@ -6,7 +6,18 @@ export function normalizeDocument(doc) {
     })
     // eleminate items, as they are stored separatly
     const { ['items']: removedItems, ...remainingDoc } = doc
+    let customer = undefined;
+    if (doc.customer) {
+        const customerId = doc.customer.customerNumber
+        customer = { ...doc.customer, id: customerId }
+        delete remainingDoc.customer
+        remainingDoc.customerId = customerId
+    }
 
-    return { document: remainingDoc, lineItems: normalizedItems }
+    return {
+        document: remainingDoc,
+        lineItems: normalizedItems,
+        customer: customer
+    }
 }
 
